b374b1f90db4be7d55d9abd7116394926c39142d
dlwls5201 <dlwls5201@naver.com>
2024-05-04 17:10:15 +0900

    add gaps, sizes dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+import 'constants/Gaps.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -7,6 +10,16 @@ void main() {
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
 
+  //home: Padding(
+  //         padding: const EdgeInsets.all(Sizes.size14),
+  //         child: Container(
+  //           child: Row(children: const [
+  //             Text('hello'),
+  //             Gaps.h20,
+  //             Text('hello'),
+  //           ]),
+  //         ),
+  //       ),
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +28,12 @@ class TikTokApp extends StatelessWidget {
         primaryColor: const Color(0xFFE9435A),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const Padding(
+        padding: EdgeInsets.all(Sizes.size14),
+        child: Row(
+          children: [Text("hello"), Gaps.h20, Text("hello")],
+        ),
+      ),
     );
   }
 }
