60827e2bd444c9c58b7125ccc1194067003ac4c4
dlwls5201 <dlwls5201@naver.com>
2024-05-15 11:38:02 +0900

    add email screen

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/Gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
 class UsernameScreen extends StatefulWidget {
@@ -29,6 +30,15 @@ class _UsernameScreenState extends State<UsernameScreen> {
   void dispose() {
     _usernameController.dispose();
     super.dispose();
+  }
+
+  void _onNextTab() {
+    if (_username.isEmpty) {
+      return;
+    }
+
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const EmailScreen()));
   }
 
   @override
@@ -81,7 +91,9 @@ class _UsernameScreenState extends State<UsernameScreen> {
               cursorColor: Theme.of(context).primaryColor,
             ),
             Gaps.v28,
-            FormButton(title: "Next", disabled: _username.isEmpty),
+            GestureDetector(
+                onTap: _onNextTab,
+                child: FormButton(title: "Next", disabled: _username.isEmpty)),
           ],
         ),
       ),
