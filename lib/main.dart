d73416d912c7d2e01409fba192029c73426994dd
dlwls5201 <dlwls5201@naver.com>
2024-07-01 00:06:42 +0900

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tiktok_clone/features/videos/repos/playback_config_repo.dart';
+import 'package:tiktok_clone/firebase_options.dart';
 import 'package:tiktok_clone/router.dart';
 
 import 'constants/sizes.dart';
@@ -10,6 +12,10 @@ import 'features/videos/view_models/playback_config_vm.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   //TODO prevent locate screen
   /*await SystemChrome.setPreferredOrientations(
